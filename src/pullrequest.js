202d4a68ad5abdc5d4e90cd43bc645084419b7ee
Samantha Gustafson <samantha.gustafson@hcl.com>
2022-11-08 14:33:51 -0500

    new

--- src/pullrequest.js
+++ src/pullrequest.js
@@ -9,46 +9,51 @@ const octokit = new Octokit({
 const ownerRepo = process.env.GITHUB_REPOSITORY.split('/');
 const owner = ownerRepo[0];
 const repo = ownerRepo[1];
-//const url =  '/repos/'+owner+'/'+repo+'/{path}'; // leave this as is
-const ref =  'heads/master/new';
-var latestCommitSHA;
+const head = 'new-branch'; //name of new branch we create
+const base = 'test03'; //name of branch of PR
 
 
-const pushContents = async () => {
+const main = async () => {
+  console.log('getting latest commit sha')
   const commits = await octokit.repos.listCommits({
       owner,
       repo,
   });
-
-  latestCommitSHA = commits.data[0].sha;
-}
+  const latestCommitSHA = commits.data[0].sha;
+  console.log(`commit sha: ${latestCommitSha}`);
+  
 //pretend user has already opened initial PR against default branch, which triggered codesweep to run
 //pretend suggested fixes were selected and copied
 
 //next step: create new branch to hold code fixes
 //to get current branch name need to parse the PR JSON for head.ref 
 // https://stackoverflow.com/questions/15096331/github-api-how-to-find-the-branches-of-a-pull-request
+  
+  /*const baseBranchRef = await octokit.git.getRef({
+    owner,
+    repo,
+    ref: `refs/heads/${baseBranch}`,
+  });*/
+  console.log(`creating branch ${head}`)
+  const newBranchRef = await octokit.git.createRef({
+    owner: owner,
+    repo: repo,
+    ref: `refs/heads/${head}`, //refs/<>/<>-withCodeFix
+    sha: baseBranchRef.data.object.sha,
+  });
 
-octokit.rest.git.createRef({
-  owner: owner,
-  repo: repo,
-  ref: ref, //refs/<>/<>-withCodeFix
-  sha: latestCommitSHA,
-});
-
-/* 
 //then: create PR to merge new branch into original
 //https://octokit.github.io/rest.js/v18#pulls-create
 
-octokit.rest.pulls.create({
-  owner: ownerRepo[0],
-  repo: ownerRepo[1],
-  head: 'test01', //new with fixes branch
-  base: 'main', //original user branch
-  title: 'This is a PR',
-  body: 'This is the body',
-}).then(data => console.log("The pullrequest was successfuly created!"))
-.catch(any){
-  data => console.log("There was an error creating the pullrequest!")
+  console.log('creating pull request');
+  octokit.rest.pulls.create({
+    owner: owner,
+    repo: repo,
+    head: head, //new with fixes branch
+    base: base, //original user branch
+    title: 'PR title',
+    body: 'PR body',
+  });
+  console.log('Pull request created');
+
 }
-*/
