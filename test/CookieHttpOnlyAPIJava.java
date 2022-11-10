9c2fc1bec0caad615ba8fb3792ba17c0cf6cf887
Samantha Gustafson <45618969+samanthagustafson@users.noreply.github.com>
2022-11-10 10:25:40 -0500

    Add files via upload

---
+++ test/CookieHttpOnlyAPIJava.java
@@ -0,0 +1,14 @@
+Cookie var;
+var.setHttpOnly(false);
+
+session.getCookie().setHttpOnly(false);
+
+myCookie.setHttpOnly(false);
+
+getCookie("sessionID", config)
+. setHttpOnly (  false  );
+
+//GOOD CODE
+var1.setHttpOnly(false);
+
+session.getCookie().setHttpOnly(true);
