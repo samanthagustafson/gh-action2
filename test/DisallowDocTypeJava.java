ff4141860c4280ca3e634ffcf3acecd356d0b1ed
Samantha Gustafson <45618969+samanthagustafson@users.noreply.github.com>
2022-11-10 10:22:39 -0500

    Delete test directory

--- test/DisallowDocTypeJava.java
+++
@@ -1,9 +0,0 @@
-// GOOD Example
-factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
-
-// BAD Example
-factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false);
-
-private static final PROPERTY = "http://apache.org/xml/features/disallow-doctype-decl";
-
-factory.setFeature(PROPERTY, false);
